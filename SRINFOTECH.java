a52cfec1ccc1c854b02d45acf770636c20b8d7f7
srinfotechbatch3 <srinfotechbatch3@gmail.com>
2025-08-05 22:54:51 +0530

    i have added srinfotech java class for to implaement the some functinality

--- SRINFOTECH.java
+++ SRINFOTECH.java
@@ -3,3 +3,9 @@ public class SRINFOTECH {
     System.out.println("SRIONFOCTEH Solutions PVt Ltd!");
   }
 }
+
+public class SRINFOTECH Solution Private Limited {
+  public static void main(String[] args) {
+    System.out.println("SRINFOTECH Solution Private Limited");
+  }
+}
